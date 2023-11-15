3a017deabc4a4721df53d5e81851ca386e7cadcc
Krishneth-25 <150888003+Krishneth-25@users.noreply.github.com>
2023-11-15 12:34:03 +0530

    Update main.py

--- CS8711/main.py
+++ CS8711/main.py
@@ -1,52 +1,13 @@
-
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def CGPA():
-    {cc=input("Enter cloud computing grade")
-    ml=input("Enter machine learning grade")
-    cns=input("Enter cybersecurity grade")
-    pom=input("Enter principles of management grade")
-    cgpa=0
-    j=cc
-    if(j==O):
-        cgpa=cgpa+10
-    elif(j==A):
-        cgpa=cgpa+9
-    else:
-        cgpa=cgpa+8
-
-     j=ml
-    if(j==O):
-        cgpa=cgpa+10
-    elif(j==A):
-        cgpa=cgpa+9
-    else:
-        cgpa=cgpa+8
-
-     j=cns
-    if(j==O):
-        cgpa=cgpa+10
-    elif(j==A):
-        cgpa=cgpa+9
-    else:
-        cgpa=cgpa+8
-
-     j=pom
-    if(j==O):
-        cgpa=cgpa+10
-    elif(j==A):
-        cgpa=cgpa+9
-    else:
-        cgpa=cgpa+8
-
-    print(cgpa)}
+def cgpa():
+    return 'HELLO WORLD'
 
 
 if __name__ == '__main__':
-    
+  
     app.run(host='127.0.0.1', port=8080, debug=True)
-
